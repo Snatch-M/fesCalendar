47ac8b29986369b133d3d55e4a0f38f68312fb4d
snatch <sunagawa.work@gmail.com>
2017-05-24 23:39:19 +0900

    [WIP]add calendar

--- src/routes/Home/components/Calendar.js
+++ src/routes/Home/components/Calendar.js
@@ -2,15 +2,33 @@ import React, { Component } from 'react';
 
 export default class Calendar extends Component {
 
-  test = () => {
-  	console.log('hoge');
+  renderCalendar = () => {
+    const weekList = ['日', '月', '火', '水', '木', '金', '土'];
+    const dateObj = new Date();
+    console.log(dateObj.getFullYear() + '年' + (dateObj.getMonth() + 1) + '月');
+    console.log(weekList[dateObj.getDay()]); // 曜日
+    dateObj.setDate(1);
+    console.log(weekList[dateObj.getDay()]); // 曜日
+    const aaa = weekList[dateObj.getDay()];
+
+    let week = [];
+    let day = [];
+    for (let i = 0; i < 7; i++) {
+      day.push(<td>{i}</td>)
+    }
+    for (let i = 0; i < 5; i++) {
+      week.push(<tr>{day}</tr>)
+    }
+
+    return week;
   }
 
   render() {
     return (
       <div>
-        {this.test()}
-        calendar
+        <table>
+          {this.renderCalendar()}
+        </table>
       </div>
     );
   }
